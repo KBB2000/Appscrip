7c931726368a75f1ab33ed598c1ec6558ff89451
KBB2000 <keshavbarkule1997@gmail.com>
2024-04-06 15:38:19 +0530

    https://github.com/KBB2000/Appscrip-task-Keshav-Barkule-

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Products from './Components/body/Products';
+import Title from './Components/body/Title';
+import Footer from './Components/footer/Footer';
+import Header from './Components/header/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Title/>
+      <Products/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
